1b2a4d1a5bb1062c7c4e59c5a8c5b5fac09e641d
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2018-10-27 19:45:46 -0300

    Proximidad
    Programación de sensor proximidad, sin utilización de librerías. Muestra de datos por serial.

---
+++ Sensor_Proximidad/Proximidad_Sin_Libs/Proximidad_Sin_Libs.ino
@@ -0,0 +1,29 @@
+const int Trigger = 6;   //Pin digital 6 para el Trigger del sensor
+const int Echo = 5;   //Pin digital 5 para el Echo del sensor
+
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+}
+
+void loop()
+{
+
+  long t; //tiempo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+}

0a1bf94cba4c6c1fb003e5a49830b9cab8e228af
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2018-10-28 22:59:57 -0300

    Code Sensor Proximidad
    Se agrega código se sensor de proximidad en clase Sensor.

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,8 +1,17 @@
 #include <SHT1x.h>
+#include <NewPing.h> //libreria para sensor de proximidad
 
 #define dataPin  2
 #define clockPin 3
+
+//seteo de pines sensor proximidad
+#define TRIGGER_PIN  6
+#define ECHO_PIN     5
+#define MAX_DISTANCE 200 //distancia maxima a identificar
+
 SHT1x sht1x(dataPin, clockPin);
+//Constructor sensor proximidad
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 class shtx{
   private:
@@ -18,6 +27,18 @@ class shtx{
         return Humedad;
       }
   };
+
+class sProximity{
+  private:
+    int Distance;
+  public:
+    int GetDistance(){
+      Distance = sonar.ping_cm(); //sonar.ping_cm() envía un ping y devuelve distancia en cm, sino hay eco de ping dentro del limite de distancia establecido entonces devuelve 0
+      return Distance;
+      // Falta incorporar algun tipo de accion cuando distancia>200cm para que el robot continue en movimiento
+    }
+};
+
 void setup()
 {
    Serial.begin(9600);
@@ -35,6 +56,12 @@ void loop()
   Serial.print(TH.GetHumedad());
   Serial.println("%");
 
+  sProximity Px; //instacia clase sProximity
+  //Muestra valores sensor de proximidad en cm
+  Serial.print("Distancia: ");
+  Serial.print(Px.GetDistance());  
+  Serial.println("cm");
+  
   delay(1000);
   
 }

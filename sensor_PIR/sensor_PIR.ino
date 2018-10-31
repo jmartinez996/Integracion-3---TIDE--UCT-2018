6cb6ca84167a8efd2cf7293618125aadf7703604
NicolOre <nicol.orellana2015@alu.uct.cl>
2018-10-31 11:19:12 -0300

    cambios pin

--- sensor_PIR/sensor_PIR.ino
+++ sensor_PIR/sensor_PIR.ino
@@ -1,6 +1,7 @@
 
 const int LEDPin= 13; // Pin de diodo LED incorporado
-const int PIRPin= 2; // Pin de entrada
+const int PIRPin= 4; // Pin de entrada
+
 void setup() {
   Serial.begin(9600);
   pinMode(LEDPin, OUTPUT); // Configuramos el pin como salida
@@ -9,13 +10,15 @@ void setup() {
 
 void loop() {
   int value= digitalRead(PIRPin); // Leemos el pin
+  
   if (value == HIGH)
-  { // Si es un valor alto:
-    digitalWrite(LEDPin, HIGH); // Encendemos el LED
-  } else
-  { // Si es un valor bajo:
-    digitalWrite(LEDPin, LOW); // Apagamos el LED
-  }
+    { // Si es un valor alto:
+      digitalWrite(LEDPin, HIGH); // Encendemos el LED
+    }
+  else
+    { // Si es un valor bajo:
+      digitalWrite(LEDPin, LOW); // Apagamos el LED
+    }
   Serial.println(value);
   delay(100);
 }

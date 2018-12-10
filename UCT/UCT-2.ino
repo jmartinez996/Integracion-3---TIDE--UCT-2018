3b31dbcf7fbfa1ec17991f3b5f5f1c550164d6d1
RafaelFuentealbaC <rafael.fuentealba2015@alu.uct.cl>
2018-12-10 00:13:58 -0300

    Update UCT-2.ino
    Se agrega correcta comunicación entre módulos de aplicación móvil con Arduino utilizando dispositivo bluetooth. Trabajo en conjunto con José Colicoy.

--- UCT/UCT-2.ino
+++ UCT/UCT-2.ino
@@ -1,16 +1,11 @@
 #include <SHT1x.h>
 #include <NewPing.h> //libreria para sensor de proximidad
+#include <Servo.h> //libreria motores servo 360
 //Set pin de temperatura y humedad
 #define dataPin  2
 #define clockPin 3
 //Set pin de Pir
 #define PIRPin 4 // Pin de entrada
-// PINES para motores
-#define  izqA 5
-#define  izqB 6
-#define  derA 9
-#define  derB 10
-int velocidad = 255;          // velocidad de los motores (0-255)
 
 //seteo de pines sensor proximidad
 #define TRIGGER_PIN  12      // pin cambiado de 6 a 12
@@ -24,6 +19,11 @@ int humedad;
 int dist;
 int piir;
 
+int pin1 = 5; //motor servo izquierdo
+int pin2 = 6; //motor servo derecho
+int stop_position = 95; //manda el servo la orden de parar
+int velocity = 30; //velocidad motor
+
 //Se utiliza en modulo ver datos de App
 String aData[4];
 String dv = ",";
@@ -32,6 +32,10 @@ String data = "";
 //Constructor sensor proximidad y temmperatura y humedad
 SHT1x sht1x(dataPin, clockPin);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+//Crea objetos servo
+Servo servoLeft;
+Servo servoRigth;
 
 class shtx{
   private:
@@ -70,54 +74,32 @@ class spir{
 };
 
 //funcion que controla movimientos en las distintas direcciones
-void Movimiento1(char mov){
-  if(mov=='a'){ //Ariba
-    digitalWrite(derB, 0);     
-    digitalWrite(izqB, 0); 
-    digitalWrite(derA, velocidad);  
-    digitalWrite(izqA, velocidad);
-    delay(500);
-    digitalWrite(derA, 0); 
-    digitalWrite(izqA, 0);  
+void MoveForward(){
+    servoLeft.write(stop_position + velocity);     
+    servoRigth.write(stop_position - velocity);         
+    delay(7000); 
+   
+    servoLeft.write(95); 
+    servoRigth.write(95); 
+    delay(3000);
   }
   
-  if(mov=='b'){ //Abajo  
-    digitalWrite(derA, 0);    
-    digitalWrite(izqA, 0);
-    digitalWrite(derB, velocidad);  
-    digitalWrite(izqB, velocidad);
-    delay(500);
-    digitalWrite(derB, 0); 
-    digitalWrite(izqB, 0);      
-  }
-  
-  if(mov=='c'){//Derecha
-    digitalWrite(derB, 0);     
-    digitalWrite(izqB, 0);
-    digitalWrite(izqA, 0);
-    digitalWrite(derA, velocidad);
-    delay(500);
-    digitalWrite(derA, 0);  
-  }
-  
-  if(mov=='d'){//Izquierda
-    digitalWrite(derB, 0);     
-    digitalWrite(izqB, 0); 
-    digitalWrite(derA, 0);  
-    digitalWrite(izqA, velocidad);
-    delay(500);
-    digitalWrite(izqA, 0);    
-  }
-  
-  if(mov=='e'){//se detiene
-    digitalWrite(derB, 0);     
-    digitalWrite(izqB, 0); 
-    digitalWrite(derA, 0);  
-    digitalWrite(izqA, 0);    
-  }  
+void MoveBack(){
+    servoLeft.write(stop_position - velocity);     
+    servoRigth.write(stop_position + velocity);         
+    delay(7000); 
+   
+    servoLeft.write(95); 
+    servoRigth.write(95); 
+    delay(3000);
+}   
 
-  //Falta definir movimientos en 45 grados
+void ShutDownMotor(){
+   servoLeft.write(95); 
+   servoRigth.write(95);
+   delay(200);
 }
+
 
 //funcion que controla movimiento por bloques
 void Movimiento2(String bloque){
@@ -128,7 +110,7 @@ void Movimiento2(String bloque){
     Serial.write(bloque[i]);
     Serial.println();**/
     if(bloque[i]!=','){
-      Movimiento1(bloque[i]);
+      //Movimiento1(bloque[i]);
     }
   }
 }
@@ -136,15 +118,13 @@ void Movimiento2(String bloque){
 
 void setup()
 {
-   Serial.begin(57600);
+  Serial.begin(57600);
    //Serial.println("iniciando");
-    pinMode(PIRPin, INPUT); // Configuramos el pin como entrada
+  pinMode(PIRPin, INPUT); // Configuramos el pin como entrada
 
     //pinMode para control motores
-    pinMode(izqA, OUTPUT);
-    pinMode(izqB, OUTPUT);
-    pinMode(derA, OUTPUT);
-    pinMode(derB, OUTPUT);
+  servoLeft.attach(pin1);
+  servoRigth.attach(pin2);
 
 }
 
@@ -165,22 +145,50 @@ void loop()
   piir=(sp.GetValue());//imprimimos el 1 si detecta movimiento y 0 si no
   aData[3] = String(piir);
   
+  char salir;
   while(Serial.available()>0){
     modulo = Serial.read();
+    while(modulo=='p' || modulo=='r' || modulo=='v'){ 
+      do{
+        //Serial.println("Estoy en p1");
+        salir = Serial.read();
+        //Comienza movimiento motores
+        if(modulo=='p'){
+          ShutDownMotor();
+          MoveForward();
+          MoveBack();
+        }
+      }while(modulo=='p' && salir!='r' && salir!='v');
+  
+      if(salir=='r' || salir=='v'){
+        modulo = salir;
+      }
+  
+      do{
+        //Serial.println("Estoy en p2");
+        salir = Serial.read();
+        if(modulo=='r'){
+          ShutDownMotor();
+        }
+      }while(modulo=='r' && salir!='p' && salir!='v');
+  
+      if(salir=='p' || salir=='v'){
+        modulo = salir;
+      }
 
-    do{
-      char estado = Serial.read();
-      Movimiento1(estado);
-    }while(modulo=='p' && modulo!='q');
-    
-    do{
-      String estado = String(Serial.read());
-      Movimiento2(estado);
-    }while(modulo=='r' && modulo!='s');
-    
-    do{
-      data = aData[0]+dv+aData[1]+dv+aData[2]+dv+aData[3];
-      Serial.println(data);
-    }while(modulo=='v' && modulo!='w');
+      do{
+        //Serial.println("Estoy en sensores");
+        salir = Serial.read();
+        if(modulo=='v'){
+          ShutDownMotor();
+          data = aData[0]+dv+aData[1]+dv+aData[2]+dv+aData[3];
+          Serial.println(data);
+        }
+      }while(modulo=='v' && salir!='p' && salir!='r');
+  
+      if(salir=='p' || salir=='r'){
+        modulo = salir;
+      }
+    }
   }
 }
